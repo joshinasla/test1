40b599d11705c5146cefbc094eed3b2801f099ee
joshinasla <joshinasla18@gmail.com>
2020-08-11 11:36:21 +0545

    Updates

---
+++ precache-manifest.1af199e7eb8c625780ac09ab0e1b5404.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e615376e41f11b4caa0a67c98676e8",
+    "url": "/http;//joshinasla.github.io/test1/index.html"
+  },
+  {
+    "revision": "f450da9d7dd5abad86f5",
+    "url": "/http;//joshinasla.github.io/test1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ade278507ccc286d5c6f",
+    "url": "/http;//joshinasla.github.io/test1/static/js/2.5eb979f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http;//joshinasla.github.io/test1/static/js/2.5eb979f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f450da9d7dd5abad86f5",
+    "url": "/http;//joshinasla.github.io/test1/static/js/main.a759a22c.chunk.js"
+  },
+  {
+    "revision": "d1b4e1a458d04dc76fa1",
+    "url": "/http;//joshinasla.github.io/test1/static/js/runtime-main.83340259.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/http;//joshinasla.github.io/test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
